0814292d1134d4c934b28ff67725bbf89ea2a7dd
Yang Liu <liuyang12@focusmedia.cn>
2020-01-15 22:52:50 +0800

    update

--- pre_process.py
+++ pre_process.py
@@ -12,26 +12,38 @@ from utils import ensure_folder
 if __name__ == "__main__":
     ensure_folder(IMG_DIR)
     imgrec = recordio.MXIndexedRecordIO(path_imgidx, path_imgrec, 'r')
+    # print(len(imgrec))
 
     samples = []
+    class_ids = set()
 
-    # %% 1 ~ 3804847
-    for i in tqdm(range(3804846)):
-        try:
+    # # %% 1 ~ 5179510
+
+    try:
+        for i in tqdm(range(10000000)):
+            # print(i)
             header, s = recordio.unpack(imgrec.read_idx(i + 1))
             img = mx.image.imdecode(s).asnumpy()
+            # print(img.shape)
             img = cv.cvtColor(img, cv.COLOR_RGB2BGR)
+            # print(header.label)
+            # print(type(header.label))
             label = int(header.label)
-            filename = '{}.png'.format(i)
+            class_ids.add(label)
+            filename = '{}.jpg'.format(i)
             samples.append({'img': filename, 'label': label})
             filename = os.path.join(IMG_DIR, filename)
             cv.imwrite(filename, img)
-        except KeyboardInterrupt:
-            raise
-        except:
-            pass
+            # except KeyboardInterrupt:
+            #     raise
+    except Exception as err:
+        print(err)
 
     with open(pickle_file, 'wb') as file:
         pickle.dump(samples, file)
 
     print('num_samples: ' + str(len(samples)))
+
+    class_ids = list(class_ids)
+    print(len(class_ids))
+    print(max(class_ids))
